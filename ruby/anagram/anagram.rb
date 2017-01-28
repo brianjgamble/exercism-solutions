e971242522834ac2c58c5673173f70ad0865c944
Brian Gamble <cc@theclearcoder.com>
2017-01-28 10:19:06 -0600

    Update anagram solution to reflect new test suite

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,5 +1,15 @@
 class Anagram < String
   def match(words)
-    words.select { |word| word.chars.sort == chars.sort }
+    words.select { |word| !is_identical?(word) && is_anagram?(word) }
+  end
+
+  private
+
+  def is_identical?(word)
+    word.downcase == downcase
+  end
+
+  def is_anagram?(word)
+    word.downcase.chars.sort == downcase.chars.sort 
   end
 end
