48c0386628c35c94dd2367b4f99cf837c1740a3a
Brian Gamble <cc@theclearcoder.com>
2017-01-28 19:57:42 -0600

    Update School to match new tests

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,22 +1,26 @@
 class School
-  def initialize(name)
-    @name = name
+  def initialize
     @students = Hash.new {|h, k| h[k] = []}
   end
   
   def add(student, grade)
     @students[grade] << student
+    sort
   end
   
-  def grade(number)
-    @students[number]
+  def students(grade_number)
+    @students[grade_number]
   end
   
   def sort
     Hash[@students.sort].each {|k,v| v.sort!}
   end
   
-  def db
-    @students
-  end  
+  def students_by_grade
+    [].tap do |students_by_grade|
+      @students.keys.sort.each do |k,v|
+        students_by_grade << {:grade => k, :students => @students[k]}
+      end
+    end
+  end
 end
