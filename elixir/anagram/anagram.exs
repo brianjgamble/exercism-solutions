46a807a66bb88a64cc2801a17d37cf67cc4d5ea3
Brian Gamble <cc@theclearcoder.com>
2017-01-29 19:02:48 -0600

    Update Anagram to match the new tests

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,16 +1,19 @@
 defmodule Anagram do
   def match(word, candidates) do
-    Enum.filter candidates, words_match?(word, &1) 
+    Enum.filter(candidates, fn(x) -> 
+      words_match?(String.downcase(word), String.downcase(x)) 
+    end)
   end
 
-  defp words_match?(_word, _word), do: false
+  defp words_match?(word, word), do: false
 
   defp words_match?(word, another_word) do
     normalize(word) == normalize(another_word) 
   end
 
   defp normalize(word) do
-    {:ok, list} = word |> String.downcase |> String.to_char_list
-    Enum.sort list
+    word 
+      |> String.to_char_list 
+      |> Enum.sort
   end
 end
