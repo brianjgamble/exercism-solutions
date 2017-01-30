d80620bb97ff92918143e83082d3dcbb9d1a6cf0
Brian Gamble <cc@theclearcoder.com>
2017-01-29 18:14:08 -0600

    Update Phrase to match new tests

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,9 +1,12 @@
 class Phrase < String
   def word_count
-    words.each_with_object(Hash.new(0)) { |word, count| count[word] += 1 }
+    words.each_with_object(Hash.new(0)) do |word, count|
+      filtered_word = word.gsub(/^'|'$/, "")
+      count[filtered_word] += 1 
+    end
   end
 
   def words
-    downcase.split(/[^a-zA-Z0-9]+/)
+    downcase.split(/[^'a-zA-Z0-9]+/)
   end
 end
